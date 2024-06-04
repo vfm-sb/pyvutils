040893e17975756289337b628bdfd6c3e9a487fa
VFM | SB <vfm_sb@proton.me>
2024-06-04 18:00:00 +0300

    Various Updates to Requirements/Dependencies & Previous Tests

--- tests/test_valid_numeric_value.py
+++ tests/test_valid_numeric_value.py
@@ -1,18 +1,18 @@
 from decimal import Decimal
-from pyvutils import valid_numeric_value
+from pyvutils import valid_numeric
 
 
 class TestValidNumericValue:
     def test_valid_numeric_value_with_valid_input(self):
-        assert valid_numeric_value(10) is True
-        assert valid_numeric_value(10.5) is True
-        assert valid_numeric_value(Decimal('10.5')) is True
+        assert valid_numeric(10) is True
+        assert valid_numeric(10.5) is True
+        assert valid_numeric(Decimal('10.5')) is True
 
-    def test_valid_numeric_value_with_invalid_input(self):
-        assert valid_numeric_value("not a number") is False
-        assert valid_numeric_value(None) is False
-        assert valid_numeric_value([]) is False
+    def test_valid_numeric_with_invalid_input(self):
+        assert valid_numeric("not a number") is False
+        assert valid_numeric(None) is False
+        assert valid_numeric([]) is False
 
-    def test_valid_numeric_value_with_numeric_string(self):
-        assert valid_numeric_value("10") is True
-        assert valid_numeric_value("10.5") is True
+    def test_valid_numeric_with_numeric_string(self):
+        assert valid_numeric("10") is True
+        assert valid_numeric("10.5") is True
